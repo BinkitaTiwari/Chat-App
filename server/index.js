9ada6348672f922e6b97ba3773dd3d10e0c4ab52
Binkita Tiwari <135starmoon@gmail.com>
2020-04-10 19:35:31 +0530

    adding some changes

--- server/index.js
+++ server/index.js
@@ -17,6 +17,8 @@ app.all('/', function(req, res, next) {
   console.log("all session")
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next()
 });
 app.use(router);
--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get("/",  (req, res) => {
   console.log("server is running")
-  res.send("Server is up and running." ).status(200);
+  res.send({response:"Server is up and running." }).status(200);
 });
 
 module.exports = router; 
